23e86203c099efafbdd724329c49c0f2d87f557e
hamdan504 <58954452+hamdan504@users.noreply.github.com>
2025-02-27 17:16:47 +0100

    Update test_app.py

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -2,7 +2,7 @@
 import unittest
 # For a simple example , we simply test True .
 class TestApp ( unittest . TestCase ):
-def test_output ( self ):
-self . assertTrue ( True )
+  def test_output ( self ):
+    self . assertTrue ( True )
 if __name__ == " __main__ ":
 unittest . main ()
