8eda81b0f1f0a2e0b0d69fe926729723d961e294
AA <ahmed3del2003@gmail.com>
2025-03-13 14:55:02 +0100

    ay

--- hello-devops/app.py
+++ hello-devops/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, CI/CD ??? step 5"
+    return "Hello, CI/CD"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
